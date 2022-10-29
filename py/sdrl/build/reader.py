66a82f0c29a7c6973a0cc9b7decab558dff5f9e6
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2022-10-29 15:34:47 +0200

    sdrl.build.reader now uses Config etc.

--- py/sdrl/build/reader.py
+++ py/sdrl/build/reader.py
@@ -2,21 +2,18 @@ import glob
 import typing as tg
 
 import base
+import sdrl.config
 import sdrl.task
 
-def read_and_check(config: base.StrAnyMap) -> sdrl.task.Tasks:
+def read_and_check(config: sdrl.config.Config):
     """Reads all task files into memory and performs consistency checking."""
-    tasks = dict()
-    for chapter in config['chapters']:
-        dirname = chapter['directory']
-        filenames = glob.glob(f"{dirname}/*.md")
-        for filename in filenames:
-            task = sdrl.task.Task(filename)
-            task.assign(dirname, "default")
-            tasks[task.taskname] = task
-    check(tasks)
-    return tasks
+    for chapter in config.chapters:
+        for taskgroup in chapter.taskgroups:
+            filenames = glob.glob(f"{config.chapterdir}/{chapter.slug}/{taskgroup.slug}/*.md")
+            for filename in filenames:
+                taskgroup.add_task(sdrl.task.Task(filename))
+    check(config)
 
     
-def check(tasks: sdrl.task.Tasks):
+def check(config: sdrl.config.Config):
     pass  # TODO 2
