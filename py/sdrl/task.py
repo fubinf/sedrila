d0944dcfadb6aa13016aa1e10d699f6778a1e9a6
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2022-10-29 15:34:08 +0200

    sdrl.task.Task adapted to fit sdrl.config style

--- py/sdrl/task.py
+++ py/sdrl/task.py
@@ -13,11 +13,11 @@ difficulty_levels = ('verylow', 'low', 'medium', 'high')
 enthusiasm_levels = ('none', 'low', 'medium', 'high')
 
 class Task:
-    file: str  # the originating pathname
+    srcfile: str  # the originating pathname
     metadata_text: str  # the entire YAML character stream
     content: str  # the entire first markdown block
     tutorcontent: str  # the entire second markdown block
-    taskname: str  # the key by which we access the Task object
+    slug: str  # the key by which we access the Task object
     
     title: str  # title: value
     description: str  # description: value (possibly multiple lines)
@@ -27,38 +27,43 @@ class Task:
     depends_on: tg.Sequence[str]  # tasknames
     todo: tg.Sequence[tg.Any]  # list of potentially YAML stuff
 
-    chaptername: str  # where the task belongs according to the config
-    taskgroupname: str  # where the task belongs according to the config
+    taskgroup: str  # where the task belongs according to the config
 
     def __init__(self, file: str, text: str=None):
         """Reads task from a file or multiline string."""
-        self.file = file
+        self.srcfile = file
         if not text:
             text = base.slurp(file)
         parts = text.split("---\n---\n")
         if len(parts) < 3:
-            raise ValueError(f"{self.file} is not a task file: must have three parts separated by double triple-dashes")
+            raise ValueError(f"{self.srcfile} is not a task file: must have three parts separated by double triple-dashes")
         self.metadata_text = parts[0]
         self.content = parts[1]
         self.tutorcontent = parts[2]  # possible further parts are internal notes and are ignored
         self._read_metadata()
     
-    def assign(self, chaptername: str, taskgroupname: str):
-        """Assign the task to a chapter and taskgroup."""
-        self.chaptername = chaptername
-        self.taskgroupname = taskgroupname
+    @property
+    def filename(self) -> str:
+        return f"{self.name}.html"
+
+    @property
+    def name(self) -> str:
+        return self.slug
+
+    def toc_link(self) -> str:
+        return f"    <a href='{self.filename}'>{self.title}</a>"
 
     def _read_metadata(self):
         """Parse and check (locally only) the metadata."""
         # ----- get taskname from filename:
-        nameparts = os.path.basename(self.file).split('.')
+        nameparts = os.path.basename(self.srcfile).split('.')
         assert len(nameparts) == 2  # taskname, suffix 'md'
-        self.taskname = nameparts[0]  # must be globally unique
+        self.slug = nameparts[0]  # must be globally unique
         try:
             #----- parse YAML data:
             metadata = yaml.safe_load(self.metadata_text)
         except yaml.YAMLError as exc:
-            logger.error(f"{self.file}: metadata YAML is malformed: {str(exc)}")
+            logger.error(f"{self.srcfile}: metadata YAML is malformed: {str(exc)}")
         try:
             #----- get mandatory attributes from YAML:
             self.title = metadata['title']
@@ -66,7 +71,7 @@ class Task:
             self.effort = metadata['effort']
             self.difficulty = metadata['difficulty']
         except AttributeError as exc:
-            logger.error(f"{self.file}: {str(exc)}")
+            logger.error(f"{self.srcfile}: {str(exc)}")
         # ----- get optional attributes from YAML:
         self.enthusiasm = getattr(metadata, 'enthusiasm', 'medium')
         self.depends_on = self._as_list(getattr(metadata, 'depends_on', []))
