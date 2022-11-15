85544049022d6cb217db3d31a6a2d235d3f7ba39
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2022-11-15 13:32:33 +0100

    sdrl.student: trivial workhours report

--- py/sdrl/student/__init__.py
+++ py/sdrl/student/__init__.py
@@ -1,4 +1,8 @@
 import argparse
+import re
+import typing as tg
+
+import sdrl.git
 
 help = """Reports on course execution so far, in particular how many hours worth of accepted tasks a student has accumulated. 
 """
@@ -9,5 +13,20 @@ def configure_argparser(subparser):
 
 
 def execute(pargs: argparse.Namespace):
-    print("'student' subcommand is not yet implemented")
-
+    commits = sdrl.git.get_commits()
+    workhours = get_workhours(commits)
+    for taskname, worktime in sorted(workhours):
+        print(f"{taskname}\n{workhours}")
+    
+    
+def get_workhours(commits: tg.Sequence[sdrl.git.Commit]) -> tg.Sequence[tg.Tuple[str, float]]:
+    """Extract all pairs of (taskname, workhours) from commit list."""
+    result = []
+    for commit in commits:
+        worktime_regexp = r"(\w+)\s+(\d+(?:\.\d+)) ?h\b"  # "MyTask117 3.5h remainder of commit msg"
+        mm = re.match(worktime_regexp, commit.subject)
+        if not mm:
+            continue  # not the format we're looking for
+        taskname, worktime = mm.group(1), float(mm.group(2))
+        result.append((taskname, worktime))
+    return result
