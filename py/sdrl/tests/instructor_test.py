33a72a6a5e4b1f69528f3f697791bb7899f78846
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2022-11-23 18:40:28 +0100

    instructor_test.py now uses tests.testbase.TempDirEnvironContextMgr

--- py/sdrl/tests/instructor_test.py
+++ py/sdrl/tests/instructor_test.py
@@ -1,62 +1,16 @@
 import os
 import shutil
-import tempfile
 import unittest.mock
-from pprint import pprint
 
 import base as b
 import git
 import sdrl.course
 import sdrl.repo as r
 import sdrl.subcmd.instructor as sut  # system under test
+from tests.testbase import TempDirEnvironContextMgr
 
 TEST_REPO = "git@github.com:fubinf/sedrila-test1.git"
 METADATA_FILE = f"{os.path.dirname(__file__)}/data/{sdrl.course.METADATA_FILE}"
-
-class TempDirEnvironContextMgr(tempfile.TemporaryDirectory):
-    """
-    Context manager which (1) creates/deletes temporary directory,
-    (2) changes into it and back to original dir,
-    (3) patches os.environ according to constructor **kwargs on entry
-    (4) adds the temporary dir as _CONTEXT_TEMPDIR, and
-    (5) unpatches environment on exit.
-    None means environment variable is not set.
-    Naively works with the default string encoding.
-    Useful for automated tests.
-    A typical usecase is a test involving the use of implicit dotfiles in $HOME.
-    """
-    def __init__(self, suffix=None, prefix=None, dir=None,
-                 **kwargs):
-        super().__init__(suffix=suffix, prefix=prefix, dir=dir)
-        self.env_patch = kwargs
-        self.origdir = os.getcwd()
-
-    def __enter__(self):
-        self.newdir = super().__enter__()
-        self.origdir = os.getcwd()
-        os.chdir(self.newdir)
-        self.env_orig = dict()
-        self.env_patch['_CONTEXT_TEMPDIR'] = self.newdir
-        for key, newvalue in self.env_patch.items():
-            origvalue = os.environ.get(key)
-            self.env_orig[key] = origvalue  # value or None
-            if newvalue is None:
-                if origvalue is not None:
-                    del os.environ[key]
-            else: 
-                os.environ[key] = newvalue
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        os.chdir(self.origdir)  # avoid deletion-related problems
-        super().__exit__(exc_type, exc_val, exc_tb)
-        for key, oldvalue in self.env_orig.items():
-            patchedvalue = os.environ.get(key)
-            if oldvalue is None:
-                if patchedvalue is not None:
-                    del os.environ[key]
-            else:
-                os.environ[key] = oldvalue
 
 
 def test_instructor_parts(capfd):
