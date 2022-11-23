6d7450742c004970055dcf30becd91d76b27b3bb
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2022-11-23 18:40:45 +0100

    repo_test.py now uses tests.testbase.TempDirEnvironContextMgr

--- py/sdrl/tests/repo_test.py
+++ py/sdrl/tests/repo_test.py
@@ -14,22 +14,9 @@ import sdrl.course
 import sdrl.repo as r
 import sdrl.subcmd.student
 
+from tests.testbase import TempDirEnvironContextMgr
+
 GIT_USER = "user@example.org"
-
-class HomeHereContextMgr:
-    def __init__(self, dir):
-        self.newdir = dir
-
-    def __enter__(self):
-        self.origdir = os.getcwd()
-        os.chdir(self.newdir)
-        self.home = os.environ['HOME']
-        os.environ['HOME'] = "."
-        return self.origdir
-    
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        os.environ['HOME'] = self.home
-        os.chdir(self.origdir)
 
 
 def create_git_repo():
@@ -39,9 +26,11 @@ def create_git_repo():
     os.system("git commit --allow-empty -m'hello'")
     os.system("git commit --allow-empty -m'A 3.25h'")
     os.system("git commit --allow-empty -m'A 0:45h'")
-    b.spit("submission.yaml", f"submission:\n A: {r.REJECT_MARK}  some comment about the problem\n")
+    b.spit("submission.yaml", f"A: {r.REJECT_MARK}  some comment about the problem\n")
     os.system("git add submission.yaml")
-    os.system("git commit -S -m'submission.yaml checked'")
+    os.system("gpgconf --kill gpg-agent")
+    os.system("gpg-agent --daemon  git commit -S -m'submission.yaml checked'")
+    # os.system("git commit -S -m'submission.yaml checked'")
     os.system("gpgconf --kill gpg-agent")
 
 
@@ -55,11 +44,11 @@ def create_gpg_key() -> str:
     return str(mm.group(1))  # the fingerprint-proper only
 
 
-@pytest.mark.skip("commit signing does not work")
+#@pytest.mark.skip("commit signing does not work")
 def test_student_work_so_far():
-    with tempfile.TemporaryDirectory() as tempdir, HomeHereContextMgr(tempdir) as origdir:
+    with TempDirEnvironContextMgr(HOME='.') as mgr:
         #----- initialize test environment:
-        course_json = b.slurp_json(f"{origdir}/py/sdrl/tests/data/{sdrl.course.METADATA_FILE}")  # config template
+        course_json = b.slurp_json(f"{mgr.origdir}/py/sdrl/tests/data/{sdrl.course.METADATA_FILE}")  # config template
         fingerprint = create_gpg_key()
         course_json['instructors'][0]['fingerprint'] = fingerprint  
         b.spit_json(sdrl.course.METADATA_FILE, course_json)  # final course config
@@ -80,7 +69,7 @@ def test_student_work_so_far():
         assert workhours_total == 4.0
         assert timevalue_total == 0.0
         assert len(entries) == 1
-        assert entries[0] == ("A", 4.0, 1.0, "R")
+        assert entries[0] == ("A", 4.0, 1.0, 1, False)
 
 
 def test_parse_taskname_workhours():
