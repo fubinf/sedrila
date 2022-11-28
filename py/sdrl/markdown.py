a6d54519f3429a986f9daf760da220860c6fe429
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2022-11-28 16:12:06 +0100

    markdown: macro_regexp: suppress matches on Markdown links

--- py/sdrl/markdown.py
+++ py/sdrl/markdown.py
@@ -31,8 +31,9 @@ extension_configs = {
     }
 }
 
-macro_regexp = r"\[([A-Z0-9_]+)(?:::(.+?))?(?:::(.+?))?\]"  
+macro_regexp = r"\[([A-Z0-9_]+)(?:::(.+?))?(?:::(.+?))?\][^(]"  
 # bracketed all-caps: [ALL2_CAPS] with zero to two arguments: [NAME::arg] or [NAME::arg1::arg2]
+# suppress matches on normal links: [TEXT](url)
 
 
 def expand_macros(markup: str) -> str:
