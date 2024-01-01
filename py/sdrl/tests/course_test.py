bbdf937ae213abecce1c6c7cf7d43e35a97da615
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2024-01-01 15:55:18 +0100

    course_test.py: fixed test_macros which broke because DIFF macro registering was moved

--- py/sdrl/tests/course_test.py
+++ py/sdrl/tests/course_test.py
@@ -1,14 +1,16 @@
 # pytest tests
 import re
 
+import base
 import sdrl.course
 import sdrl.markdown as md
 
 def test_macros(capsys):
+    md.register_macro('DIFF', 1, sdrl.course.Task.expand_diff)
     expansion = md.render_markdown("(none)", "difficult [DIFF::4] morestuff", 
-                                   mode="-", blockmacro_topmatter=dict())
+                                   mode=base.Mode.STUDENT, blockmacro_topmatter=dict())
     print(expansion)
     assert re.search(r"difficult.+Difficulty: high", expansion)
     md.render_markdown("(none)", "[DIFF::44]", 
-                       mode="-", blockmacro_topmatter=dict())  # non-existing level
+                       mode=base.Mode.STUDENT, blockmacro_topmatter=dict())  # non-existing level
     assert "in range" in capsys.readouterr().out
