a4b766c9ce0426cf7b7ad066b1ad1e895fdcc0e1
myname <myname@mynery.eu>
2024-03-30 14:23:56 +0100

    provide hint about exiting subshell after interactive mode, allow opening of task pages

--- py/sdrl/interactive.py
+++ py/sdrl/interactive.py
@@ -1,5 +1,6 @@
 from blessed import Terminal
 import typing as tg
+import webbrowser
 
 import base as b
 import sdrl.repo as r
@@ -15,11 +16,15 @@ def prefix(entry: r.ReportEntry, selected: dict[str, bool], rejected: dict[str, 
     return " "
 
 
-def redraw_filter_selection(term: Terminal, entries: tg.Sequence[r.ReportEntry], rowindex: int, selected: dict[str, bool], rejected: dict[str, bool]):
+def redraw_filter_selection(term: Terminal, entries: tg.Sequence[r.ReportEntry], rowindex: int, selected: dict[str, bool], rejected: dict[str, bool], course_url: str):
     print(term.home + term.clear, end="")
+    lines = term.height - 1
     print("Move with arrow keys, select/deselect with space/enter, exit with escape")
-    rangestart = max(0, rowindex - term.height / 2)
-    rangeend = min(len(entries), rowindex + term.height / 2)
+    if not(course_url is None):
+        print("Press 'o' to open task in browser")
+        lines = lines - 1
+    rangestart = max(0, rowindex - lines / 2)
+    rangeend = min(len(entries), rowindex + lines / 2)
     for i in range(rangestart, rangeend):
         if i == rowindex:
             print(term.reverse, end="")
@@ -30,13 +35,13 @@ def redraw_filter_selection(term: Terminal, entries: tg.Sequence[r.ReportEntry],
     print(term.normal, end="") #if we end on the selection
 
 
-def filter_entries(entries: tg.Sequence[r.ReportEntry], selected: dict[str, bool], rejected: dict[str, bool]):
+def filter_entries(entries: tg.Sequence[r.ReportEntry], selected: dict[str, bool], rejected: dict[str, bool], course_url: str):
     term = Terminal()
     rowindex = 0
     with term.cbreak(), term.hidden_cursor():
         inp = None
         while not(inp) or not(str(inp) == "q" or str(inp.name) == "KEY_ESCAPE"):
-            redraw_filter_selection(term, entries, rowindex, selected, rejected)
+            redraw_filter_selection(term, entries, rowindex, selected, rejected, course_url)
             inp = term.inkey()
             if str(inp) == " " or str(inp.name) == "KEY_ENTER":
                 if rejected is None:
@@ -54,20 +59,22 @@ def filter_entries(entries: tg.Sequence[r.ReportEntry], selected: dict[str, bool
             elif str(inp) == "k" or str(inp.name) == "KEY_UP":
                 if rowindex > 0:
                     rowindex -= 1
+            elif not(course_url is None) and (str(inp) == "o" or str(inp) == "O"):
+                webbrowser.open(f"{course_url}/{entries[rowindex][0]}.html")
 
 
 def select_entries(entries: tg.Sequence[r.ReportEntry]):
     selected = {entry[0]: True for entry in entries}
-    filter_entries(entries, selected, None)
+    filter_entries(entries, selected, None, None)
     return list(filter(lambda entry: selected[entry[0]], entries))
 
 
 #marks entries as accepted if they are, returns rejections as the count might not be enough to know whether they were rejected in current run
-def grade_entries(entries: tg.Sequence[r.ReportEntry]):
+def grade_entries(entries: tg.Sequence[r.ReportEntry], course_url: str):
     submission = b.slurp_yaml(r.SUBMISSION_FILE)
     selected = {entry[0]: (submission.get(entry[0]) or "").startswith(r.ACCEPT_MARK) for entry in entries}
     rejected = {entry[0]: (submission.get(entry[0]) or "").startswith(r.REJECT_MARK) for entry in entries}
-    filter_entries(entries, selected, rejected)
+    filter_entries(entries, selected, rejected, course_url)
     if not(any(selected.values())) and not(any(rejected.values())):
         return None
     for i in range(len(entries)):
--- py/sdrl/subcmd/instructor.py
+++ py/sdrl/subcmd/instructor.py
@@ -53,17 +53,18 @@ def execute(pargs: argparse.Namespace):
     r.compute_student_work_so_far(course)
     entries, workhours_total, timevalue_total = r.student_work_so_far(course)
     opentasks = rewrite_submission_file(course, r.SUBMISSION_FILE)
-    entries = list(filter(lambda entry: entry[0] in opentasks and course.task(entry[0]).remaining, entries))
+    entries = list(filter(lambda entry: entry[0] in opentasks and course.task(entry[0]).remaining_attempts > 0, entries))
     if not(pargs.check):
         b.info("Don't run check, just prepare commit")
     elif not(entries):
         b.info("No tasks to check found. Assuming check already done. Preparing commit.")
     elif pargs.interactive:
-        rejections = i.grade_entries(entries)
+        rejections = i.grade_entries(entries, student.course_url)
         if rejections is None:
             b.info("Nothing selected, nothing to do")
             return
         b.spit_yaml(r.SUBMISSION_FILE, r.submission_file_entries(course, entries, rejections))
+        subshell_exit_info(reminder=True)
     else:
         call_instructor_cmd(course, instructor_cmd(), pargs, iteration=0)
     if pargs.put:
@@ -117,14 +118,22 @@ def rewrite_submission_entries(course: sdrl.course.Course, entries: b.StrAnyDict
             entries[taskname] += f" (previously rejected {task.rejections}x)"
 
 
+def subshell_exit_info(reminder: bool = False):
+    if reminder and not(os.environ.get(b.SEDRILA_COMMAND_ENV)):
+        return #explicit call, no subshell. no need to provide that info
+    if reminder:
+        b.info("You are still inside a subshell command!")
+    b.info("Exit that command (e.g. the shell) to trigger automatic commit+push")
+    b.info(f"of the modified {r.SUBMISSION_FILE}.")
+
+
 def call_instructor_cmd(course: sdrl.course.Course, cmd: str, pargs: argparse.Namespace = None, iteration: int = 0):
     """Calls user-set command as indicated by environment variables"""
     if iteration == 0:
         b.info(f"Will now call the command given in the {USER_CMD_VAR} environment variable or else")
         b.info(f"the command given in the SHELL environment variable or else '{USER_CMD_DEFAULT}'.")
         b.info(f"The resulting command in your case will be:\n  '{cmd}'")
-        b.info("Exit that command (e.g. the shell) to trigger automatic commit+push")
-        b.info(f"of the modified {r.SUBMISSION_FILE}.")
+        subshell_exit_info()
         b.info(f"Please change status of tasks in {r.SUBMISSION_FILE} to either {r.ACCEPT_MARK}\nor {r.REJECT_MARK} accordingly.")
         b.info("You can also just run `sedrila` to get an interactive list.")
         b.info("Feel free to add remarks at the end of the accept/reject lines.")
