b9a559f77d7e5839915bbdebb37902da8ff7930e
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2024-06-25 15:20:41 +0200

    cache.py: State, self.written, __getitem__(), __setitem__(), item()

--- py/cache.py
+++ py/cache.py
@@ -18,6 +18,17 @@ import dbm
 import itertools
 import os
 import time
+import typing as tg
+
+import base as b
+
+# states of Elements wrt a Product or wrt the cache
+class State(str):
+    pass  # a trivial type
+
+NONEXISTING = 'nonexisting'
+HAS_CHANGED = 'has_changed'
+AS_BEFORE = 'as_before'
 
 LIST_SEPARATOR = '|'  # separates entries in list-valued dbm entries. Symbol is forbidden in all names.
 USE_CACHE_FLAG_KEY = 'SEDRILA_USE_CACHE'  # name of environment var for activating cache (temporary until completion)
@@ -30,6 +41,7 @@ ITREEDIR_FILELIST_KEY = '__itreedir_files__'  # complete filesnames in itreedir 
 
 class SedrilaCache:
     db: object  # a dbm._Database
+    written: b.StrAnyDict  # what was written into cache since start
     timestamp_start: int
     timestamp_cached: int
     chapterdir_samefiles: list[str]
@@ -39,21 +51,52 @@ class SedrilaCache:
         self.timestamp_start = int(time.time())
         self.timestamp_cached = int(self.db.get(LAST_BUILD_TIMESTAMP_KEY, "0"))  # default to "everything is old"
         self.db = dbm.open(cache_filename, flag='c')  # open or create dbm file
+        self.written = dict()
         self.chapterdir_samefiles, self.chapterdir_newfiles = (
                 self._scandir(chapterdir, self.db.get(CHAPTERDIR_FILELIST_KEY, "")))  # TODO 2: altdir, itreedir
 
+    def __contains__(self, key: str) -> bool:
+        return key in self.written or key in self.db
 
-    def __getstate__(self):
+    def __getitem__(self, key: str) -> tg.Any:
+        if key in self.written:
+            return self.written[key]
+        elif key in self.db:
+            return self.db[key]
+        raise ValueError(key)
+
+    def __setitem__(self, key: str, value: tg.Any):
+        self.written[key] = value
+
+    def __getstate__(self):  # for pickle
         return None  # SedrilaCache has no state that should be pickled
 
-    def __setstate__(self, state):
+    def __setstate__(self, state):  # for unpickle
         pass  # no useful state is restored upon unpickle; one must create a new instance
     
     def finish(self):
         """Finalize the cache: Bring it up-to-date for the next run."""
         self.db[LAST_BUILD_TIMESTAMP_KEY] = str(self.timestamp_start)
+        for key, value in self.written.items():
+            self.db[key] = value
         self._storefilelist(CHAPTERDIR_FILELIST_KEY, self.chapterdir_samefiles, self.chapterdir_newfiles)
         # TODO 2: _storefilelist for altdir, itreedir
+
+    def item(self, key: str) -> tuple[tg.Any, State]:
+        if key in self.written:
+            return (self.written[key], HAS_CHANGED)
+        elif key in self.db:
+            return (self.db[key], AS_BEFORE)
+        else:
+            return (None, NONEXISTING)
+
+    def state(self, key: str) -> State:
+        if key in self.written:
+            return HAS_CHANGED
+        elif key in self.db:
+            return AS_BEFORE
+        else:
+            return NONEXISTING
 
     def _scandir(self, dirname: str, cached_filelist: str) -> tuple[list[str], list[str]]:
         """Lists of all (samefiles, newfiles) below dirname according to cached_filelist and timestamp_cached."""
