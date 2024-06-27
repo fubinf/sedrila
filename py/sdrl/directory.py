80859338cdbe74e812d16d31d8a85a530901eb94
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2024-06-27 15:34:23 +0200

    directory.py: Elements registry/factory (and future build orchestrator)

---
+++ py/sdrl/directory.py
@@ -0,0 +1,49 @@
+"""Combined Elements registry/factory and build orchestrator."""
+
+
+class Directory:
+    """
+    Each Element is registered here and can be accessed by type and name. 
+    The Builder knows when to build each and builds them in an order such that 
+    when a Product gets built, the state of all its dependencies is already known.
+    This order is also fully determined by the Element types alone.
+    """
+    def __init__(self):
+        import sdrl.elements as el
+        import sdrl.course as course
+        import sdrl.glossary as glossary
+        self.managed_types = [
+            # Each has a downcased dict attribute use by get_the()/make_the().
+            # The ordering is the build ordering:
+            el.Sourcefile, el.Outputfile,
+            el.Zipdir, el.Zipfile,
+            el.Content, el.Topmatter,
+            el.Body_s, el.Body_i, el.IncludeList, el.PartrefList,
+            el.AssumedByList, el.RequiredByList,
+            el.Tocline, el.Toc,
+            course.Course, course.Chapter, course.Taskgroup, course.Task, glossary.Glossary,
+        ]
+
+        for thistype in self.managed_types:
+            dictname = str(thistype).lower()
+            self.__setattr__(dictname, dict())
+        self.sourcefile = dict()
+
+    def get_the(self, mytype: type, name: str) -> 'sdrl.elements.Element':
+        the_dict = self._getdict(mytype)
+        return the_dict.get(name, None)
+
+    def make_the(self, mytype: type, name: str, *args, **kwargs) -> 'sdrl.elements.Element':
+        the_dict = self._getdict(mytype)
+        assert name not in the_dict  # if we re-make the same object, the logic is broken
+        instance = mytype(*args, **kwargs)
+        the_dict[name] = instance
+        return instance
+
+    def make_or_get_the(self, mytype: type, name: str, *args, **kwargs) -> 'sdrl.elements.Element':
+        instance = self.get_the(mytype, name)
+        return instance if instance else self.make_the(mytype, name, *args, **kwargs)
+
+    def _getdict(self, thetype: type):
+        dictname = str(thetype).lower()
+        return getattr(self, dictname)
