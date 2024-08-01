f29f1d1d912b084714bc53d258fda061dc2877f0
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2024-08-01 18:05:10 +0200

    participant.py: add Student.metadatadict and .get_metadata()

--- py/sdrl/participant.py
+++ py/sdrl/participant.py
@@ -1,22 +1,31 @@
 import os
 
+import requests
+
 import base as b
 
 PARTICIPANT_FILE = "student.yaml"
 
 
 class Student:
+    root: str  # where PARTICIPANT_FILE lives: '.' or '..'
+    course_url: str  # SeDriLa homepage URL minus the '/index.html' part
+    student_name: str
+    student_id: str
+    partner_student_name: str
+    partner_student_id: str
+    metadata_url: str  # where to get JSON config
+
     def __init__(self):
-        root = "."
+        self.root = "."
         try:
             # grant students some slack. if they are calling from inside a working dir, it's fine.
             if not os.path.isfile(PARTICIPANT_FILE) and os.path.isfile(os.path.join("..", PARTICIPANT_FILE)):
-                root = ".."
-            data = b.slurp_yaml(os.path.join(root, PARTICIPANT_FILE))
+                self.root = ".."
+            data = b.slurp_yaml(os.path.join(self.root, PARTICIPANT_FILE))
         except FileNotFoundError:
             b.critical(f"cannot read '{PARTICIPANT_FILE}'")
         try:
-            self.root = root
             self.course_url = data['course_url']  # noqa
             assert isinstance(self.course_url, str)
             self.student_name = data['student_name']
@@ -28,6 +37,24 @@ class Student:
         except KeyError:
             b.critical(f"malformed file '{PARTICIPANT_FILE}': must contain "
                        "course_url, student_name, student_id, partner_student_name, partner_student_id.")
+        self.metadata_url = f"{self.course_url}/{b.METADATA_FILE}"
+
+    @property
+    def metadatadict(self) -> b.StrAnyDict:
+        return self.get_metadata(self.course_url)
+        
+    @staticmethod    
+    def get_metadata(course_url: str) -> b.StrAnyDict:
+        url = os.path.join(course_url, b.METADATA_FILE)
+        try:
+            resp = requests.get(url=url)
+        except:  # noqa
+            b.critical(f"Error fetching URL '{url}'.")
+        try:
+            metadata = resp.json()
+        except:  # noqa
+            b.critical(f"JSON format error at '{url}'")
+        return metadata
 
     @staticmethod
     def prompts(pdict):
