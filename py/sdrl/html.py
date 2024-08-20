93324f25cd66bfdab8ec06d32c8bc2402e1160cd
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2024-08-20 10:33:19 +0200

    html.py: introduce semantic markup: <nav> for breadcrumb

--- py/sdrl/html.py
+++ py/sdrl/html.py
@@ -12,7 +12,7 @@ def as_attribute(text: str) -> str:
 def breadcrumb(*args):
     """Renders breadcrumb HTML fragment from list of items with breadcrumb_item property."""
     SEPARATOR = " &gt; "
-    return '<div><span class="breadcrumbs">%s</span></div>' % SEPARATOR.join([arg.breadcrumb_item for arg in args])
+    return '<nav><span class="breadcrumbs">%s</span></nav>' % SEPARATOR.join([arg.breadcrumb_item for arg in args])
 
 
 def difficulty_symbol(level: int) -> str:
