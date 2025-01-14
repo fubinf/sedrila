4f2815f3148781dc74ec4d271f9f3969828e2ee2
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2025-01-14 11:21:51 +0100

    participant.py: allow arbitrary root dirs

--- py/sdrl/participant.py
+++ py/sdrl/participant.py
@@ -9,7 +9,7 @@ import sdrl.constants as c
 
 
 class Student:
-    root: str  # where PARTICIPANT_FILE lives: '.' or '..'
+    root: str  # where PARTICIPANT_FILE lives
     course_url: str  # SeDriLa homepage URL minus the '/index.html' part
     student_name: str
     student_id: str
@@ -17,12 +17,10 @@ class Student:
     partner_student_id: str
     metadata_url: str  # where to get JSON config
 
-    def __init__(self):
-        self.root = "."
+    def __init__(self, rootdir='.'):
+        self.root = rootdir
         try:
-            # grant students some slack. if they are calling from inside a working dir, it's fine.
-            if not os.path.isfile(c.PARTICIPANT_FILE) and os.path.isfile(os.path.join("..", c.PARTICIPANT_FILE)):
-                self.root = ".."
+            self._adjust_root()
             data = b.slurp_yaml(os.path.join(self.root, c.PARTICIPANT_FILE))
         except FileNotFoundError:
             b.critical(f"cannot read '{c.PARTICIPANT_FILE}'")
@@ -36,14 +34,28 @@ class Student:
             assert isinstance(self.partner_student_name, str)
             self.partner_student_id = str(data['partner_student_id'])
         except KeyError:
-            b.critical(f"malformed file '{c.PARTICIPANT_FILE}': must contain "
+            b.critical(f"malformed file '{c.PARTICIPANT_FILE}': must contain strings "
                        "course_url, student_name, student_id, partner_student_name, partner_student_id.")
         self.metadata_url = f"{self.course_url}/{c.METADATA_FILE}"
 
     @functools.cached_property
     def metadatadict(self) -> b.StrAnyDict:
         return self.get_metadata(self.course_url)
-        
+    
+    @property
+    def participantfile_path(self) -> str:
+        return os.path.join(self.root, c.PARTICIPANT_FILE)
+    
+    @staticmethod
+    def dummy_participant() -> 'Student':
+        """Pseudo-Student with no root and no course_url/metadata_url."""
+        result = Student()
+        result.student_name = "N.N."
+        result.student_id = "some_id"
+        result.partner_student_name = "-"
+        result.partner_student_id = "-"
+        return result
+
     @staticmethod    
     def get_metadata(course_url: str) -> b.StrAnyDict:
         FILE_URL_PREFIX = 'file://'
@@ -70,3 +82,11 @@ class Student:
         pdict.setdefault('partner_student_name', "Partner's name")
         pdict.setdefault('partner_student_id', "Partner's ID")
         return pdict
+
+    def _adjust_root(self):
+        # grant students some slack. if they are calling from inside a working dir, it's fine.
+        participantfile_found = os.path.isfile(self.participantfile_path)
+        root_is_relative_path = not os.path.isabs(self.root)
+        participantfile_found_one_level_higher = os.path.isfile(os.path.join('..', self.participantfile_path))
+        if not participantfile_found and root_is_relative_path and participantfile_found_one_level_higher:
+            self.root = ".."  # use PARTICIPANT_FILE from superdir
