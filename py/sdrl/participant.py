435b98d499d1ac450da22f943a489a33e3e50d26
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2025-01-28 10:44:00 +0100

    participant.py: Student: course_with_work, is_writable, filter_submissions()

--- py/sdrl/participant.py
+++ py/sdrl/participant.py
@@ -1,3 +1,4 @@
+import contextlib
 import functools
 import json
 import os
@@ -5,10 +6,18 @@ import os
 import requests
 
 import base as b
+import git
 import sdrl.constants as c
+import sdrl.course
+import sdrl.repo as r
 
 
 class Student:
+    """
+    Represents the content of c.PARTICIPANT_FILE. Can initialize that file.
+    Can represent and modify c.SUBMISSION_FILE.
+    Modified versions are kept in {student_gituser}-{c.SUBMISSION_FILE} in superdir.
+    """
     PROMPT_CONFIG_ATTR = 'student_yaml_attribute_prompts'
     STUDENT_YAML_PROMPT_DEFAULTS = dict(
         course_url="URL of course homepage: ",
@@ -24,13 +33,14 @@ class Student:
     student_gituser: str
     partner_gituser: str
 
-    def __init__(self, rootdir='.'):
-        self.root = rootdir
+    def __init__(self, rootdir='.', with_submission=False):
+        self.root = rootdir = rootdir.rstrip('/')
+        # ----- read c.PARTICIPANT_FILE:
         try:
-            self._adjust_root()
-            data = b.slurp_yaml(os.path.join(self.root, c.PARTICIPANT_FILE))
+            data = b.slurp_yaml(self.participantfile_path)
         except FileNotFoundError:
-            b.critical(f"cannot read '{c.PARTICIPANT_FILE}'")
+            b.critical(f"cannot read '{self.participantfile_path}'")
+        # ----- interpret contents:
         try:
             self.course_url = str(data['course_url'])  # noqa
             self.student_name = str(data['student_name'])
@@ -38,13 +48,25 @@ class Student:
             self.student_gituser = str(data['student_gituser'])
             self.partner_gituser = str(data['partner_gituser'])
         except KeyError:
-            b.critical(f"malformed file '{c.PARTICIPANT_FILE}': must contain strings " +
+            b.critical(f"malformed file '{self.participantfile_path}': must contain strings " +
                        str([key for key in self.STUDENT_YAML_PROMPT_DEFAULTS]))
         homepage_explicitname = "index.html"
         if self.course_url.endswith(f"/{homepage_explicitname}"):
             self.course_url = self.course_url[:-len(homepage_explicitname)]  # leave only directory path
         if not self.course_url.endswith("/"):
             self.course_url += "/"  # make sure directory path ends with slash
+        # ----- read c.SUBMISSION_FILE:
+        if not with_submission or not os.path.isfile(self.submissionfile_path):
+            self.submission = dict()
+            return
+        self.submission = b.slurp_yaml(self.submissionfile_path)
+        self.filter_submission()
+        if self.is_writable:
+            self.save_submission()
+
+    @functools.cached_property
+    def course(self) -> sdrl.course.CourseSI:
+        return sdrl.course.CourseSI(configdict=self.course_metadata, context=self.course_metadata_url)
 
     @functools.cached_property
     def course_metadata(self) -> b.StrAnyDict:
@@ -54,10 +76,43 @@ class Student:
     def course_metadata_url(self) -> str:
         return self.get_course_metadata_url(self.course_url)
 
+    @functools.cached_property
+    def course_with_work(self) -> sdrl.course.CourseSI:
+        with contextlib.chdir(self.root):
+            commits = git.commits_of_local_repo(reverse=True)
+            r.compute_student_work_so_far(self.course, commits)
+        return self.course
+
+    @property
+    def is_writable(self) -> bool:
+        return True
+
     @property
     def participantfile_path(self) -> str:
         return os.path.join(self.root, c.PARTICIPANT_FILE)
-    
+
+    @property
+    def submissionfile_path(self) -> str:
+        return os.path.join(self.root, c.SUBMISSION_FILE)
+
+    @property
+    def submission_backup_path(self) -> str:
+        return os.path.join(self.root, '..', c.SUBMISSION_FILE)
+
+    def filter_submission(self):
+        """Kick out non-existing and rejected-for-good tasks and emit warnings."""
+        submission1 = dict(**self.submission)  # constant copy (we will delete entries)
+        file = self.submissionfile_path  # abbrev
+        for taskname, status in submission1.items():
+            task = self.course_with_work.task(taskname)
+            if not task:
+                b.warning(f"{file}: '{taskname}' is not a taskname. Ignored.")
+                del self.submission[taskname]
+            elif task.remaining_attempts < 0:
+                b.warning(f"{file}: '{taskname}' has remaining_attempts = {task.remaining_attempts}. Ignored.")
+                del self.submission[taskname]
+
+        
     @classmethod    
     def get_course_metadata(cls, course_url: str) -> b.StrAnyDict:
         FILE_URL_PREFIX = 'file://'
@@ -81,6 +136,18 @@ class Student:
     def get_course_metadata_url(cls, course_url: str) -> str:
         return os.path.join(course_url, c.METADATA_FILE)
 
+    def save_submission(self):
+        b.spit_yaml(self.submission_backup_path, self.submission)
+
+
+class StudentReadonly(Student):
+    """
+    Represents the content of c.PARTICIPANT_FILE. Can initialize that file.
+    """
+    @property
+    def is_writable(self) -> bool:
+        return False
+
     @classmethod
     def build_participant_file(cls):
         b.info(f"Your following inputs will populate the file '{c.PARTICIPANT_FILE}'.")
@@ -99,15 +166,3 @@ class Student:
         b.info(f"Wrote '{c.PARTICIPANT_FILE}'.")
         b.info("If you made any mistake, correct it with an editor now.")
         b.info("Then commit the file and push the commit.")
-
-    def _adjust_root(self):
-        # grant students some slack. if they are calling from inside a working dir, it's fine.
-        participantfile_found = os.path.isfile(self.participantfile_path)
-        root_is_relative_path = not os.path.isabs(self.root)
-        participantfile_found_one_level_higher = os.path.isfile(os.path.join('..', self.participantfile_path))
-        if not participantfile_found and root_is_relative_path and participantfile_found_one_level_higher:
-            self.root = ".."  # use PARTICIPANT_FILE from superdir
-
-
-class Students:
-    ...
