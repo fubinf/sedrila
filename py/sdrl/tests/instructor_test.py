b5fecefcc1fc179ecca9b8ee11cfd388533bfb4b
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2025-01-31 14:32:21 +0100

    instructor_test.py: complete the integration test

--- py/sdrl/tests/instructor_test.py
+++ py/sdrl/tests/instructor_test.py
@@ -1,3 +1,4 @@
+import contextlib
 import os
 import shutil
 import unittest.mock
@@ -8,83 +9,97 @@ import base as b
 import git
 import sdrl.constants as c
 import sdrl.course
+import sdrl.participant
 import sdrl.repo as r
 import sdrl.subcmd.instructor as sut  # system under test
+
 import tests.testbase as tb
 
 TEST_REPO = "git@github.com:fubinf/sedrila-test1.git"
 METADATA_FILE = f"{os.path.dirname(__file__)}/data/{c.METADATA_FILE}"
 
+student_yaml = """
+course_url: file://../coursedir
+student_name: Me Student
+student_id: 12345
+student_gituser: mestudent
+partner_gituser: ""
+"""
 
-@pytest.mark.xfail(reason="needs updating")
 def test_instructor_parts(capfd):
     """
-    Deep-integrationey test. Accesses external server, creates+deletes directories etc.
+    Tests sdrl.repo, sdrl.participant, sdrl.instructor.
+    Deep-integrationey test: Accesses external server, creates+deletes directories etc.
     sut.execute() is not well-suited for testing, so we test its constituent parts here.
     Not pretty due to the redundancy, but does the job OK.
     """
+    # TODO 2: Move to participant_test.py. Kick out parts of repo_test.py?
     with tb.TempDirEnvironContextMgr(**{c.REPO_USER_CMD_VAR: "echo SEDRILA_INSTRUCTOR_COMMAND was called"}) as mgr:
         os.environ[c.REPOS_HOME_VAR] = mgr.newdir  # will not be unpatched; not a problem
+        
         # ----- test clone:
         b._testmode_reset()
-        sut.checkout_student_repo(TEST_REPO, home=mgr.newdir)  # will clone
+        os.mkdir("instructordir")
+        os.chdir("instructordir")
+        git.clone(TEST_REPO, "studentdir")
         assert "Cloning into" in capfd.readouterr().err
-        assert os.getcwd().endswith(git.username_from_repo_url(TEST_REPO))
-        os.chdir(mgr.newdir)
-        # ----- test pull:
-        b._testmode_reset()
-        sut.checkout_student_repo(TEST_REPO, home=mgr.newdir)  # will pull and get "Already up to date."
-        assert "Already up to date" in capfd.readouterr().out
-        assert os.getcwd().endswith(git.username_from_repo_url(TEST_REPO))
-        # ----- make pseudo-coursedir:
-        os.mkdir('out')
-        shutil.copy(METADATA_FILE, 'out')
-        # ----- read data from repo:
-        b._testmode_reset()
-        configfile = f"out/{c.METADATA_FILE}"
-        course = sdrl.course.CourseSI(configdict=b.slurp_json(configfile), context=configfile)
-        commits = git.commits_of_local_repo(reverse=True)
-        r.compute_student_work_so_far(course, commits)
-        entries, workhours_total, timevalue_total = r.student_work_so_far(course)
-        assert entries[0] == r.ReportEntry('Task1', 'basis/linuxcli/Task1', 1.0, 1.0, 0, False)
-        # ----- rewrite submission file:
-        # the repo's submission file contains dict(Task1="CHECK", NonExistingTask3="CHECK")
-        b._testmode_reset()
-        sut.rewrite_submission_file(course, c.SUBMISSION_FILE)
-        submission = b.slurp_yaml(c.SUBMISSION_FILE)
-        assert submission['NonExistingTask3'] == c.SUBMISSION_NONTASK_MARK
-        # ----- simulate invalid user editing attempt:
-        with unittest.mock.patch('time.sleep'):
-            sut.call_instructor_cmd(course, sut.instructor_cmd(), iteration=0)
-        output = capfd.readouterr().out
-        print(output)
-        if False:  # due to the kludge with USER_CMD_VAR above , this part is broken 
-            assert f"{c.REPO_USER_CMD_VAR} was called" in output  # check some of the explanation
-            assert os.environ[c.REPO_USER_CMD_VAR] in output  # make sure the actual command is shown
-        is_valid = sut.validate_submission_file(course, c.SUBMISSION_FILE)
-        assert not is_valid
-        output = capfd.readouterr().out
-        assert "has neither" in output
-        assert "Impossible mark" in output
-        # ----- simulate valid user editing attempt:
-        del submission['NonExistingTask3']  # delete nonsense entry as the instructor must
-        submission['Task1'] = c.SUBMISSION_ACCEPT_MARK  # accept the other entry
-        b.spit_yaml(c.SUBMISSION_FILE, submission)
-        with unittest.mock.patch('time.sleep'):
-            sut.call_instructor_cmd(course, sut.instructor_cmd(), iteration=1)
-        output = capfd.readouterr().out
-        if False:  # due to the kludge with USER_CMD_VAR above , this part is broken 
-            assert f"Calling '{os.environ[c.REPO_USER_CMD_VAR]}' again" in output  # the repeat blurb
-        assert sut.validate_submission_file(course, c.SUBMISSION_FILE)
-        # ----- finish:
+        with open("studentdir/student.yaml", 'wt') as f:
+            f.write(student_yaml)
         
-        def pseudo_push():
-            assert last_commit.hash != git.commits_of_local_repo()[0].hash
-            git.discard_commits(1)  # undo the c.SUBMISSION_FILE commit
-        last_commit = git.commits_of_local_repo()[0]  # remember it to check the test itself
-        with unittest.mock.patch('git.push', new=pseudo_push):
-            sut.commit_and_push(c.SUBMISSION_FILE)
-        output = capfd.readouterr().out
-        assert "submission.yaml checked" in output
-        last_commit2 = git.commits_of_local_repo()[0]
-        assert last_commit.hash == last_commit2.hash  # has pseudo_push done the discard correctly?
+        # ----- make pseudo-coursedir:
+        os.mkdir('coursedir')
+        shutil.copy(METADATA_FILE, 'coursedir')
+        # with contextlib.chdir("studentdir"):
+        #     os.system("git log|cat")
+        
+        # ----- test Context:
+        ctx = sdrl.participant.make_context(None, ["studentdir"], with_submission=True, show_size=True)
+        student = ctx.studentlist[0]  # there is only one
+        assert ctx.submission_tasknames == {'Task1'}
+        assert student.submission_tasknames == {'Task1'}
+        assert "/task/Task1.md" in ctx.submission_pathset
+        assert "/task/Task1.md" in student.submission_pathset
+        assert "/task/Task3.md" not in ctx.submission_pathset
+        assert "/task/Task3.md" not in student.submission_pathset
+        assert "/task/Task3.md" in ctx.pathset
+        assert "/task/Task3.md" in student.pathset
+        task1, task2 = student.course_with_work.task('Task1'), student.course_with_work.task('Task2')
+        assert (task1.timevalue, task2.timevalue) == (1.0, 2.0)
+        assert (task1.workhours, task2.workhours) == (1.0, 0.5)
+        assert not task1.is_accepted 
+        
+        # ----- perform accepting/rejecting:
+        submission = dict(Task1=c.SUBMISSION_CHECK_MARK, Task2=c.SUBMISSION_CHECK_MARK)
+        filename = os.path.join(student.topdir, c.SUBMISSION_FILE)
+        b.spit_yaml(filename, submission)
+        ctx = sdrl.participant.make_context(None, ["studentdir"], with_submission=True, show_size=True)
+        student = ctx.studentlist[0]  # now with the above submission file
+        print("#1:", student.submission)
+        while True: # horrible logic here, but natural in the webapp:
+            state = student.move_to_next_state('Task1', student.submission['Task1'])
+            if state == c.SUBMISSION_ACCEPT_MARK:
+                break
+        print("#2:", student.submission)
+        assert student.submission['Task1'] == c.SUBMISSION_ACCEPT_MARK
+        while True:
+            state = student.move_to_next_state('Task2', student.submission['Task2'])
+            if state == c.SUBMISSION_REJECT_MARK:
+                break
+        print("#3:", student.submission)
+        assert student.submission['Task2'] == c.SUBMISSION_REJECT_MARK  # ditto in submission.yaml
+        with contextlib.chdir(student.topdir):  # make unsigned commit, we will mock the signature check
+            git.commit(*[c.SUBMISSION_FILE], msg=c.SUBMISSION_CHECKED_COMMIT_MSG, signed=False)
+        
+        # ----- check result of accepting/rejecting:
+        def all_signers_allowed(commit, allowed_signers):
+            print("all_signers_allowed:", commit)
+            return True
+        with unittest.mock.patch('sdrl.repo.is_allowed_signer', new=all_signers_allowed):  # believe everything
+            ctx = sdrl.participant.make_context(None, ["studentdir"], with_submission=True, show_size=True)
+        student = ctx.studentlist[0]  # there is only one
+        print("#4:", student.submission)
+        task1, task2 = student.course_with_work.task('Task1'), student.course_with_work.task('Task2')
+        assert (task1.workhours, task2.workhours) == (1.0, 0.5)
+        assert task1.is_accepted 
+        assert not task2.is_accepted
+        assert (task1.rejections, task2.rejections) == (0, 1)
