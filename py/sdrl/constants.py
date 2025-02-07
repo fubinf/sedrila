f940d683ed9c2e1950d69e55ee0808a7628a3c92
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2025-02-07 13:47:24 +0100

    constants.py: EDITOR_CMD_DEFAULT, SUBMISSION_NONCHECK_MARK

--- py/sdrl/constants.py
+++ py/sdrl/constants.py
@@ -3,6 +3,7 @@ AUTHOR_CONFIG_FILENAME = "sedrila.yaml"  # at top-level of source dir
 AUTHOR_GLOSSARY_BASENAME = "glossary"  # .md at top-level of chapterdir, .html in build directory
 AUTHOR_OUTPUT_INSTRUCTORS_DEFAULT_SUBDIR = "instructor"
 CACHE_FILENAME = ".sedrila_cache"  # author: in instructor target dir
+EDITOR_CMD_DEFAULT = "/usr/bin/nano"
 EVENTCACHE_FILENAME = ".sedrila_events"  # evaluator: in respos_dir
 HTML_DIFFICULTY_SIGN = "&#x26ab;&#xfe0e;"  # &#x26ab; is an icon and always black, &#xfe0e; is the text-variant selector
   # https://commons.wikimedia.org/wiki/Unicode_circle_shaped_symbols
@@ -17,7 +18,8 @@ REPO_USER_CMD_DEFAULT = "/bin/bash"  # fallback only if $SHELL is not set
 SUBMISSION_FILE = "submission.yaml"
 SUBMISSION_COMMIT_MSG = "submission.yaml"
 SUBMISSION_CHECKED_COMMIT_MSG = "submission.yaml checked"
-SUBMISSION_CHECK_MARK = "CHECK"
+SUBMISSION_CHECK_MARK = "CHECK"  # beware when renaming: there are same-named CSS styles
+SUBMISSION_NONCHECK_MARK = "NONCHECK"
 SUBMISSION_ACCEPT_MARK = "ACCEPT"
 SUBMISSION_REJECT_MARK = "REJECT"
 SUBMISSION_OVERRIDE_PREFIX = "OVERRIDE_"
