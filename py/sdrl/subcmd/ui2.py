fb84ae311251491f8899161a13a1f1ab80b755ed
davih05 <davih05@zedat.fu-berlin.de>
2026-03-11 14:31:21 +0100

    ui2: added ui2 subcommand with temporary integration into legacy ui and implemented new student subcommand

--- py/sdrl/subcmd/ui2.py
+++ py/sdrl/subcmd/ui2.py
@@ -1,4 +1,28 @@
 
 
+import argparse
+import click
+
+from .student import student_command
+import base as b
+
+# for command to show up in old system
+meaning = """Temporary subcommand for the new CLI"""
+def add_arguments(_): pass
+def execute(_: argparse.Namespace): assert False, "this is only intended as a dummy"
+# ---------------------------
+
+@click.group()
+@click.version_option()
+@click.option(
+    "--log", default="INFO",
+    help="Log level for logging to stdout",
+    type=click.Choice([*b.loglevels.keys()])
+)
+def ui2(log):
+    b.set_loglevel(log)
+    print("hi from ui2")
+
+ui2.add_command(student_command)
 
 
